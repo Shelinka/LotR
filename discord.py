e1dbc35b2fbd9831af87bdefd84727b2686e6ba4
Shelinka <89917663+Shelinka@users.noreply.github.com>
2023-12-09 23:54:24 +0100

    Update discord.py

--- discord.py
+++ discord.py
@@ -22,3 +22,30 @@ async def on_message_delete(message):
 
 # Run the bot with the token
 bot.run('YOUR_BOT_TOKEN')
+
+--
+second implementation
+
+import discord
+from discord.ext import commands
+
+intents = discord.Intents(messages=True, guilds=True, members=True)
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name} ({bot.user.id})')
+    print('------')
+
+@bot.event
+async def on_message_delete(message):
+    user = message.author
+    await user.send(f"You deleted a message: '{message.content}'")
+
+# Define a slash command for /ping
+@bot.command(name="ping", description="Get the current ping of the bot.")
+async def ping(ctx):
+    latency = bot.latency * 1000  # Convert to milliseconds
+    await ctx.send(f'Pong! Current ping is {latency:.2f} ms.')
+
+bot.run('YOUR_BOT_TOKEN')
