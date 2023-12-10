531faa1ccb3f53568e156bcfe99bcb71803fd265
Shelinka <89917663+Shelinka@users.noreply.github.com>
2023-12-10 01:47:26 +0100

    Update discord.py

--- discord.py
+++ discord.py
@@ -2,7 +2,7 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
-bot = comamnds.Bot(command_prefix="!", intents = discord.Intents.all())
+bot = commands.Bot(command_prefix="!", intents = discord.Intents.all())
 
 #intents = discord.Intents(messages=True, guilds=True, members=True)
 #bot = commands.Bot(command_prefix='!', intents=intents)
@@ -10,21 +10,18 @@ bot = comamnds.Bot(command_prefix="!", intents = discord.Intents.all())
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name} ({bot.user.id})')
-    try:
-        synced = await bot.tree.sync()
-        print(f"Synced {len(synced)} command(s)")
-    except Exception as e:
-        print(e)
-
-
-# Define a slash command for /ping
-@client.command()
-async def ping(ctx):
-     await ctx.send(f'Pong! In {round(client.latency * 1000)}ms')
 
 @bot.event
 async def on_message_delete(message):
     user = message.author
     await user.send(f"Heya! Please do not delte your messages in LFG channels. In case you delisted your group etc. you can always use the Edit function to let others know that your group is full. Thank you <3")
+
+@client.command()
+async def ping(ctx):
+    before = time.monotonic()
+    message = await ctx.send("Pong!")
+    ping = (time.monotonic() - before) * 1000
+    await message.edit(content=f"Pong!  `{int(ping)}ms`")
+
 
 bot.run('YOUR_BOT_TOKEN')
